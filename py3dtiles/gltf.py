f4fbc611b9a3c5984567a905caad9bfd7f08838a
Jérémy Gaillard <jeremy.gaillard@oslandia.com>
2017-01-09 11:50:39 +0100

    Transform support and build proper object instead of string for header

--- py3dtiles/gltf.py
+++ py3dtiles/gltf.py
@@ -4,6 +4,7 @@ import binascii
 import math
 import triangle
 import numpy as np
+import json
 
 class GlTF(object):
 
@@ -22,7 +23,6 @@ class GlTF(object):
 
     @staticmethod
     def from_wkb(wkbs, bboxes, transform):
-        # TODO: handle transform
         """
         Parameters
         ----------
@@ -33,7 +33,7 @@ class GlTF(object):
             Array of bounding boxes (numpy.array)
 
         transform : numpy.array
-            World coordinates transformation matrix
+            World coordinates transformation flattend matrix
 
         Returns
         -------
@@ -82,7 +82,7 @@ class GlTF(object):
         return glTF
 
 def outputbglTF(binVertices, binIndices, binNormals, nVertices, nIndices, bb):
-    scene = outputJSON(binVertices, binIndices, binNormals, nVertices, nIndices, bb, True)
+    scene = json.dumps(outputJSON(binVertices, binIndices, binNormals, nVertices, nIndices, bb, True), separators=(',', ':'))
 
     scene = struct.pack(str(len(scene)) + 's', scene.encode('utf8'))
     # body must be 4-byte aligned
@@ -106,7 +106,7 @@ def outputBin(binVertices, binIndices, binNormals):
     binary = binary + b''.join(binIndices)
     return binary
 
-def outputJSON(binVertices, binIndices, binNormals, nVertices, nIndices, bb, bgltf, uri = "data:,"):
+def outputJSON(binVertices, binIndices, binNormals, nVertices, nIndices, bb, bgltf, uri = ""):
     # Buffer
     meshNb = len(binVertices)
     sizeIdx = []
@@ -115,144 +115,124 @@ def outputJSON(binVertices, binIndices, binNormals, nVertices, nIndices, bb, bgl
         sizeVce.append(len(binVertices[i]))
         sizeIdx.append(len(binIndices[i]))
 
-    uriStr = uri
+    buffers = {
+        'binary_glTF': {
+            'byteLength': 2 * sum(sizeVce) + sum(sizeIdx),
+            'type': "arraybuffer"
+        }
+    }
     if uri != "":
-        uriStr = ',"uri": "{0}"'.format(uri)
-    buffers = """\
-"binary_glTF": {{
-    "byteLength": {0},
-    "type": "arraybuffer"
-}}""".format(2 * sum(sizeVce) + sum(sizeIdx))
+        buffers["binary_glTF"]["uri"] = uri
 
     # Buffer view
-    bufferViews = """\
-"BV_indices": {{
-    "buffer": "binary_glTF",
-    "byteLength": {0},
-    "byteOffset": {2},
-    "target": 34963
-}},
-"BV_vertices": {{
-    "buffer": "binary_glTF",
-    "byteLength": {1},
-    "byteOffset": 0,
-    "target": 34962
-}},
-"BV_normals": {{
-    "buffer": "binary_glTF",
-    "byteLength": {1},
-    "byteOffset": {1},
-    "target": 34962
-}}""".format(sum(sizeIdx), sum(sizeVce), 2 * sum(sizeVce))
+    bufferViews = {
+        'BV_indices': {
+            'buffer': "binary_glTF",
+            'byteLength': sum(sizeIdx),
+            'byteOffset': 2 * sum(sizeVce),
+            'target': 34963
+        },
+        'BV_vertices': {
+            'buffer': "binary_glTF",
+            'byteLength': sum(sizeVce),
+            'byteOffset': 0,
+            'target': 34962
+        },
+        'BV_normals': {
+            'buffer': "binary_glTF",
+            'byteLength': sum(sizeVce),
+            'byteOffset': sum(sizeVce),
+            'target': 34962
+        },
+    }
 
     # Accessor
-    accessors = ""
+    accessors = {}
     for i in range(0, meshNb):
-        bbmin = str(bb[i][0][1]) + ',' + str(bb[i][0][2]) + ',' + str(bb[i][0][0])
-        bbmax = str(bb[i][1][1]) + ',' + str(bb[i][1][2]) + ',' + str(bb[i][1][0])
-        accessors = accessors + """\
-"AI_{0}": {{
-    "bufferView": "BV_indices",
-    "byteOffset": {1},
-    "byteStride": 2,
-    "componentType": 5123,
-    "count": {3},
-    "type": "SCALAR"
-}},
-"AV_{0}": {{
-    "bufferView": "BV_vertices",
-    "byteOffset": {2},
-    "byteStride": 12,
-    "componentType": 5126,
-    "count": {4},
-    "max": [{5}],
-    "min": [{6}],
-    "type": "VEC3"
-}},
-"AN_{0}": {{
-    "bufferView": "BV_normals",
-    "byteOffset": {2},
-    "byteStride": 12,
-    "componentType": 5126,
-    "count": {4},
-    "max": [1,1,1],
-    "min": [-1,-1,-1],
-    "type": "VEC3"
-}},""".format(i, sum(sizeIdx[0:i]), sum(sizeVce[0:i]), nIndices[i], nVertices[i], bbmax, bbmin)
-    accessors = accessors[0:len(accessors)-1]
+        accessors["AI_" + str(i)] = {
+            'bufferView': "BV_indices",
+            'byteOffset': sum(sizeIdx[0:i]),
+            'byteStride': 2,
+            'componentType': 5123,
+            'count': nIndices[i],
+            'type': "SCALAR"
+        }
+        accessors["AV_" + str(i)] = {
+            'bufferView': "BV_vertices",
+            'byteOffset': sum(sizeVce[0:i]),
+            'byteStride': 12,
+            'componentType': 5126,
+            'count': nVertices[i],
+            'max': [bb[i][0][1], bb[i][0][2], bb[i][0][0]],
+            'min': [bb[i][1][1], bb[i][1][2], bb[i][1][0]],
+            'type': "VEC3"
+        }
+        accessors["AN_" + str(i)] = {
+            'bufferView': "BV_normals",
+            'byteOffset': sum(sizeVce[0:i]),
+            'byteStride': 12,
+            'componentType': 5126,
+            'count': nVertices[i],
+            'max': [1,1,1],
+            'min': [-1,-1,-1],
+            'type': "VEC3"
+        }
 
     # Meshes
-    meshes = ""
+    meshes = {}
     for i in range(0, meshNb):
-        meshes = meshes + """\
-"M{0}": {{
-    "primitives": [{{
-        "attributes": {{
-            "POSITION": "AV_{0}",
-            "NORMAL": "AN_{0}"
-        }},
-        "indices": "AI_{0}",
-        "material": "defaultMaterial",
-        "mode": 4
-    }}]
-}},""".format(i)
-
-    meshes = meshes[0:len(meshes)-1]
+        meshes["M" + str(i)] = {
+            'primitives': [{
+                'attributes': {
+                    "POSITION": "AV_" + str(i),
+                    "NORMAL": "AN_" + str(i)
+                },
+                "indices": "AI_" + str(i),
+                "material": "defaultMaterial",
+                "mode": 4
+            }]
+        }
 
     # Nodes
-    meshesId = ""
-    for i in range(0, meshNb):
-        meshesId += '"M{0}",'.format(i)
-    meshesId = meshesId[0:len(meshesId)-1]
+    nodes = {
+        'node': {
+            'matrix': [float(e) for e in transform],
+            'meshes': ["M" + str(i) for i in range(0,meshNb)]
+        }
+    }
+    # TODO: one node per feature would probably be better
 
-    nodes = ""
-    nodes = nodes + """\
-"node": {{
-    "meshes": [{0}]
-}}""".format(meshesId)
-
-    # Extension
-    extension = ""
+    # Extensions
+    extensions = []
     if bgltf:
-        extension = """,\
-"extensionsUsed" : [
-    "KHR_binary_glTF"
-]"""
+        extensions.append("KHR_binary_glTF")
 
     # Final glTF
-    JSON = """\
-{{
-    "scene": "defaultScene",
-    "scenes": {{
-        "defaultScene": {{
-            "nodes": [
-                "node"
-            ]
-        }}
-    }},
-    "nodes": {{
-        {0}
-    }},
-    "meshes": {{
-        {1}
-    }},
-    "accessors": {{
-        {2}
-    }},
-    "bufferViews": {{
-        {3}
-    }},
-    "buffers": {{
-        {4}
-    }},
-    "materials": {{
-        "defaultMaterial": {{
-            "name": "None"
-        }}
-    }}{5}
-}}""".format(nodes, meshes, accessors, bufferViews, buffers, extension)
+    header = {
+        'scene': "defaultScene",
+        'scenes': {
+            'defaultScene': {
+                'nodes': [
+                    "node"
+                ]
+            }
+        },
+        'nodes': nodes,
+        'meshes': meshes,
+        'accessors': accessors,
+        'bufferViews': bufferViews,
+        'buffers': buffers,
+        'materials': {
+            'defaultMaterial': {
+                'name': "None"
+            }
+        }
+    }
 
-    return JSON
+    if len(extensions) != 0:
+        header["extensionsUsed"] = extensions
+
+    return header
 
 
 def indexation(triangles, normals):
@@ -267,7 +247,7 @@ def indexation(triangles, normals):
     maxIdx = 0
 
     for i in range(0, len(triangles)):
-        n = normals[i]#struct.pack('fff', *normals[i])
+        n = normals[i]
         for pt in triangles[i]:
             if (n.tostring(),pt.tostring()) in index:
                 indices.append(index[(n.tostring(),pt.tostring())])
@@ -377,11 +357,16 @@ if __name__ == "__main__":
     wkb = binascii.unhexlify("01f70300000f00000001eb03000001000000050000000000240857001fc0000000f0c16b1dc000514b73bb89fe3f0000c0b4a83f1fc00000805ad3fcf7bf00514b73bb89fe3f0000c0b4a83f1fc00000805ad3fcf7bf40ba11164d6e00c00000240857001fc0000000f0c16b1dc040ba11164d6e00c00000240857001fc0000000f0c16b1dc000514b73bb89fe3f01eb030000010000000500000000003023f5be0f40000040800ced1cc000514b73bb89fe3f0000240857001fc0000000f0c16b1dc000514b73bb89fe3f0000240857001fc0000000f0c16b1dc040ba11164d6e00c000003023f5be0f40000040800ced1cc040ba11164d6e00c000003023f5be0f40000040800ced1cc000514b73bb89fe3f01eb030000010000000500000000003023f5be0f40000040800ced1cc040ba11164d6e00c00000f8c951400f400000c09bfd01f6bf40ba11164d6e00c00000f8c951400f400000c09bfd01f6bf00514b73bb89fe3f00003023f5be0f40000040800ced1cc000514b73bb89fe3f00003023f5be0f40000040800ced1cc040ba11164d6e00c001eb03000001000000050000000000d8f6769b21400000c0b12e2ef5bf40ba11164d6e00c000002477d86c2140000010527e321d4040ba11164d6e00c000002477d86c2140000010527e321d4000895fb14e6e00400000d8f6769b21400000c0b12e2ef5bf00895fb14e6e00400000d8f6769b21400000c0b12e2ef5bf40ba11164d6e00c001eb030000010000000500000000002477d86c2140000010527e321d4040ba11164d6e00c000009a5ab67e21c000006024b4751c4040ba11164d6e00c000009a5ab67e21c000006024b4751c4000895fb14e6e004000002477d86c2140000010527e321d4000895fb14e6e004000002477d86c2140000010527e321d4040ba11164d6e00c001eb030000010000000500000000005ab9175021c0000080685721f8bf00895fb14e6e004000009a5ab67e21c000006024b4751c4000895fb14e6e004000009a5ab67e21c000006024b4751c4040ba11164d6e00c000005ab9175021c0000080685721f8bf40ba11164d6e00c000005ab9175021c0000080685721f8bf00895fb14e6e004001eb03000001000000040000000000c0b4a83f1fc00000805ad3fcf7bf00895fb14e6e00400000dc09f67720c00000c0e7150ff8bf40ba11164d6e00c00000c0b4a83f1fc00000805ad3fcf7bf00514b73bb89fe3f0000c0b4a83f1fc00000805ad3fcf7bf00895fb14e6e004001eb03000001000000040000000000c0b4a83f1fc00000805ad3fcf7bf00514b73bb89fe3f0000dc09f67720c00000c0e7150ff8bf40ba11164d6e00c00000c0b4a83f1fc00000805ad3fcf7bf40ba11164d6e00c00000c0b4a83f1fc00000805ad3fcf7bf00514b73bb89fe3f01eb03000001000000040000000000dc09f67720c00000c0e7150ff8bf40ba11164d6e00c000005ab9175021c0000080685721f8bf00895fb14e6e004000005ab9175021c0000080685721f8bf40ba11164d6e00c00000dc09f67720c00000c0e7150ff8bf40ba11164d6e00c001eb03000001000000040000000000c0b4a83f1fc00000805ad3fcf7bf00895fb14e6e004000005ab9175021c0000080685721f8bf00895fb14e6e00400000dc09f67720c00000c0e7150ff8bf40ba11164d6e00c00000c0b4a83f1fc00000805ad3fcf7bf00895fb14e6e004001eb03000001000000040000000000d8f6769b21400000c0b12e2ef5bf00895fb14e6e0040000054698b6b1940000000ad1698f5bf40ba11164d6e00c00000d8f6769b21400000c0b12e2ef5bf40ba11164d6e00c00000d8f6769b21400000c0b12e2ef5bf00895fb14e6e004001eb03000001000000040000000000d8f6769b21400000c0b12e2ef5bf00895fb14e6e00400000f8c951400f400000c09bfd01f6bf00895fb14e6e0040000054698b6b1940000000ad1698f5bf40ba11164d6e00c00000d8f6769b21400000c0b12e2ef5bf00895fb14e6e004001eb0300000100000004000000000054698b6b1940000000ad1698f5bf40ba11164d6e00c00000f8c951400f400000c09bfd01f6bf00895fb14e6e00400000f8c951400f400000c09bfd01f6bf00514b73bb89fe3f000054698b6b1940000000ad1698f5bf40ba11164d6e00c001eb0300000100000004000000000054698b6b1940000000ad1698f5bf40ba11164d6e00c00000f8c951400f400000c09bfd01f6bf00514b73bb89fe3f0000f8c951400f400000c09bfd01f6bf40ba11164d6e00c0000054698b6b1940000000ad1698f5bf40ba11164d6e00c001eb03000001000000050000000000f8c951400f400000c09bfd01f6bf00895fb14e6e00400000c0b4a83f1fc00000805ad3fcf7bf00895fb14e6e00400000c0b4a83f1fc00000805ad3fcf7bf00514b73bb89fe3f0000f8c951400f400000c09bfd01f6bf00514b73bb89fe3f0000f8c951400f400000c09bfd01f6bf00895fb14e6e0040")
 
     box = [[-8.74748499994166, -7.35523200035095, -2.05385796777344], [8.8036420000717, 7.29930999968201, 2.05386103222656]]
-    transform = np.identity(4, dtype=float) # TODO: translation : 1842015.125, 5177109.25, 247.87364196777344
+    transform = np.array([
+        [1,0,0,1842015.125],
+        [0,1,0,5177109.25],
+        [0,0,1,247.87364196777344],
+        [0,0,0,1]], dtype=float) # translation : 1842015.125, 5177109.25, 247.87364196777344
+    transform = transform.flatten('F')
     glTF = GlTF.from_wkb([wkb], [box], transform)
 
     f = open("test.gltf", 'w')
-    f.write(glTF.header)
+    f.write(json.dumps(glTF.header, separators=(',', ':')))
     f.close()
     f = open("test.bin", 'bw')
     f.write(bytes(glTF.body))
