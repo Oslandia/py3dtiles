965e90202aa3d6a85863ca9839dcf86104d3d331
Jérémy Gaillard <jeremy.gaillard@oslandia.com>
2018-02-27 18:33:06 +0100

    Increment version to 1.0.1

--- py3dtiles/__init__.py
+++ py3dtiles/__init__.py
@@ -9,6 +9,6 @@ from .b3dm import B3dm
 from .batch_table import BatchTable
 from .wkb_utils import TriangleSoup
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __all__ = ['TileReader', 'convert_to_ecef', 'Tile', 'Feature', 'GlTF', 'Pnts',
            'B3dm', 'BatchTable', 'TriangleSoup']
