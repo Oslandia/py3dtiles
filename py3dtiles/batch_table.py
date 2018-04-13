5ed3006ed0026db86a341d14b96fcc4159c30b4b
Jérémy Gaillard <jeremy.gaillard@oslandia.com>
2018-04-13 15:46:09 +0200

    batch table hierarchy WIP

--- py3dtiles/batch_table.py
+++ py3dtiles/batch_table.py
@@ -13,9 +13,45 @@ class BatchTable(object):
 
     def __init__(self):
         self.header = {}
+        self.classToIndex = {}
 
     def add_property_from_array(self, propertyName, array):
         self.header[propertyName] = array
+
+    # Hierarchy functions
+    def _init_hierarchy(self):
+        self.header['HIERARCHY'] = {
+            'classes': [],
+            'instancesLength': 0,
+            'classIds': [],
+            'parentCounts': [],
+            'parentIds': []
+        }
+
+    # propertyNames: array of strings
+    def add_class(self, name, propertyNames):
+        if 'HIERARCHY' not in self.header:
+            self._init_hierarchy()
+        self.header['HIERARCHY']['classes'].append({
+            'name': name,
+            'length': 0,
+            'instances': {propertyName: [] for propertyName in propertyNames}
+        })
+        self.classToIndex[name] = len(self.classToIndex)
+
+    # properties: dictionnary with the same attributes as class instances
+    # parent indices refers to the index of the parent in the batch table
+    def add_class_instance(self, className, properties, parentIndices):
+        idx = self.classToIndex[className]
+        myClass = self.header['HIERARCHY']['classes'][idx]
+        myClass['length'] += 1
+        for i in myClass['instances']:
+            myClass['instances'][i].append(properties[i])
+        self.header['HIERARCHY']['instancesLength'] += 1
+        self.header['HIERARCHY']['classIds'].append(idx)
+        self.header['HIERARCHY']['parentCounts'].append(len(parentIndices))
+        self.header['HIERARCHY']['parentIds'].extend(parentIndices)
+
 
     # returns batch table as binary
     def to_array(self):
