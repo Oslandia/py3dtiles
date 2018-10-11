4efa0a17301b57590b43636335d86aa352ff11d4
Éric Lemoine <eric.lemoine@oslandia.com>
2018-10-11 08:51:39 +0200

    Change license from LGPL2 to Apache 2 in setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     url='https://github.com/Oslandia/py3dtiles',
     author='Oslandia',
     author_email='contact@oslandia.com',
-    license='LGPL2 or later',
+    license='Apache License Version 2.0',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
