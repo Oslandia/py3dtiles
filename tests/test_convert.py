288d8f9619a62d50791c63cc8c69be397ebeb480
Augustin Trancart <augustin.trancart@oslandia.com>
2019-12-10 10:20:16 +0100

    Test: add a basic test for convert

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
-
-import unittest
+import os
+from pytest import approx
+import shutil
 
 from py3dtiles import convert_to_ecef
+from py3dtiles.convert import convert
 
 
-class TestConvert(unittest.TestCase):
+def test_convert_to_ecef():
+    # results tested with gdaltransform
+    [x, y, z] = convert_to_ecef(-75.61200462622627,
+                                40.03886513981721,
+                                2848.448771114095,
+                                4326)
+    approx(x, 1215626.30684538)
+    approx(y, -4738673.45914053)
+    approx(z, 4083122.83975827)
 
-    def test_convert(self):
 
-        # results tested with gdaltransform
-        [x, y, z] = convert_to_ecef(-75.61200462622627,
-                                    40.03886513981721,
-                                    2848.448771114095,
-                                    4326)
-        self.assertAlmostEqual(x, 1215626.30684538)
-        self.assertAlmostEqual(y, -4738673.45914053)
-        self.assertAlmostEqual(z, 4083122.83975827)
+def test_convert():
+    # just
+    convert(os.path.join(os.path.dirname(os.path.abspath(__file__)), './ripple.las'), outfolder='./tmp')
+    assert os.path.exists(os.path.join('tmp', 'tileset.json'))
+    assert os.path.exists(os.path.join('tmp', 'r0.pnts'))
+    shutil.rmtree('./tmp')
+
+
+
